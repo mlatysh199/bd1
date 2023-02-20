423d6c2fc03afda81ba1b1639b813dc9dc350f16
Max Latysh <maxlatysh@Maxs-MacBook-Air.local>
2023-02-20 10:21:42 -0600

    Preparación de mvn

---
+++ weather-report/src/main/java/tec/bd1/tareas/weatherreport/main/WeatherReportApp.java
@@ -0,0 +1,15 @@
+package tec.bd1.tareas.weatherreport.main;
+
+/**
+ * La clase inicial.
+ */
+public class WeatherReportApp {
+    /**
+     * Función principal.
+     * 
+     * @param args los argumentos iniciales
+     */
+    public static void main(String[] args) {
+        
+    }
+}
